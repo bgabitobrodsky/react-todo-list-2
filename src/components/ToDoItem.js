e028ca6db1a375d0fa73f9df68739852a6b6a21f
Bernardo Gabito <bernardogabito@gmail.com>
2022-06-15 17:57:23 -0300

    animations

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const ToDoItem = ({data:{description:desc,datetime,color,done}, setData}) => {
+    
+    const [className, setClassName] = useState(()=>{
+        if(new Date().toLocaleString() === datetime){
+            return 'todo-item animate__animated animate__backInLeft animate__faster'
+        }else{
+            return 'todo-item';
+        }
+    })
 
     const handleRemove = (e) => {
-        setData((data) => {
-            const newData = data.filter(item => item.datetime !== datetime);
-            localStorage.setItem('data',JSON.stringify(newData));
-            return newData;
-        })
+        setClassName('todo-item animate__animated animate__backOutRight')
+        setTimeout(()=>{
+            setData((data) => {
+                const newData = data.filter(item => item.datetime !== datetime);
+                localStorage.setItem('data',JSON.stringify(newData));
+                return newData;
+            })
+        },360)
     }
 
     const handleDone = (e) => {
@@ -18,34 +29,31 @@ export const ToDoItem = ({data:{description:desc,datetime,color,done}, setData})
             localStorage.setItem('data',JSON.stringify(newData));
             return newData;
         })
+        setClassName('todo-item animate__animated animate__tada')
     }
-
-  return (
-    <div className='todo-item' style={{backgroundColor: color}}>
-        <div className='todo-item-desc'>
-            <h3>
-                {desc}
-            </h3>
-            <p>
-                {datetime}
-            </p>
+    
+    return (
+        <div className={className} style={{backgroundColor: color}}>
+            <div className='todo-item-desc'>
+                <h3>{desc}</h3>
+                <p>{datetime}</p>
+            </div>
+            <div className='buttons'>
+                {
+                    !done? (
+                        <button className='btn btn-success' onClick={handleDone}>
+                            Done
+                        </button>
+                    ):(
+                        <span className='mr-2'>
+                            Done!
+                        </span>
+                    )
+                }
+                <button className='btn btn-danger ml-2' onClick={handleRemove}>
+                    Remove
+                </button>
+            </div>
         </div>
-        <div className='buttons'>
-            {
-                !done? (
-                    <button className='btn btn-success' onClick={handleDone}>
-                        Done
-                    </button>
-                ):(
-                    <span className='mr-2'>
-                        Done!
-                    </span>
-                )
-            }
-            <button className='btn btn-danger ml-2' onClick={handleRemove}>
-                Remove
-            </button>
-        </div>
-    </div>
-  )
+    )
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -3,8 +3,8 @@ import { ToDoItem } from './ToDoItem'
 
 export const ToDoList = ({data, setData}) => {
   return (
-    <>
+    <div className='todo-grid'>
         {data?.map((item)=> <ToDoItem key={item.datetime} data={item} setData={setData}></ToDoItem>)}
-    </>
+    </div>
   )
 }
